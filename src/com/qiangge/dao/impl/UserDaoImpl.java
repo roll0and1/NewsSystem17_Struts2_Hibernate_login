6d4bb0bec5331a7a6218a13d16825725809f4131
zhazha <542291186@qq.com>
2015-09-10 21:48:30 +0800

    add HibernateUtil 类

--- src/com/qiangge/dao/impl/UserDaoImpl.java
+++ src/com/qiangge/dao/impl/UserDaoImpl.java
@@ -1,9 +1,5 @@
 package com.qiangge.dao.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -16,13 +12,13 @@ import org.hibernate.service.ServiceRegistryBuilder;
 import com.qiangge.dao.UserDao;
 import com.qiangge.model.User;
 import com.qiangge.utils.AppException;
-import com.qiangge.utils.DBUtil;
+import com.qiangge.utils.HibernateUtil;
 
 public class UserDaoImpl implements UserDao {
 
 	public boolean isExist(String name) throws AppException {
 		// 操作标志
-		boolean flag = true;
+		boolean flag = false;
 		try {
 			// 定义hql语句，根据用户名查询记录数，？为占位符
 			String hql = "from User u where u.del=0 and u.name=?";
@@ -31,19 +27,19 @@ public class UserDaoImpl implements UserDao {
 			Configuration cfg = new Configuration().configure();
 			sf = cfg.buildSessionFactory(new ServiceRegistryBuilder()
 					.applySettings(cfg.getProperties()).buildServiceRegistry());
-			System.out.println("sf:"+sf);
+			System.out.println("sf:" + sf);
 			// 2.获取session
 			Session session = sf.openSession();
-			System.out.println("session:"+session);
+			System.out.println("session:" + session);
 			// 3.获取query对象
 			Query query = session.createQuery(hql);
-			System.out.println("query:"+query);
+			System.out.println("query:" + query);
 			// 设置占位符
 			query.setString(0, name);
 			// 4.进行查询
 			List list = query.list();
-			
-			System.out.println("Size:"+list.size());
+
+			System.out.println("Size:" + list.size());
 			if (list.size() > 0) {// 如果size>0，表示存在该用户，修改标识符为true
 				flag = true;
 			}
@@ -92,62 +88,55 @@ public class UserDaoImpl implements UserDao {
 		return flag;
 	}
 
+	/**
+	 * 通过判断用户id来确定用户可否登录
+	 */
 	@Override
 	public int login(String name, String password) throws AppException {
 		// 初始化用户id
 		int id = -1;
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		conn = DBUtil.getConnection(); // 创建数据库连接
-		// 声明操作语句：将用户信息保存到数据库中， “？”为占位符 注意del
-		String sql = "select id from t_user where name=? and password=? and del=0;";
+		// 获取session
 		try {
-			psmt = conn.prepareStatement(sql);
-			psmt.setString(1, name);
-			psmt.setString(2, password);
-			rs = psmt.executeQuery();
-			while (rs.next()) {
-				id = rs.getInt(1);
+			Session session = HibernateUtil.getSession();
+			String hql = "from User u where u.name=? and u.password=? and u.del=0";
+			Query query = session.createQuery(hql);
+			query.setString(0, name);
+			query.setString(1, password);
+			User u = (User) query.uniqueResult();
+			if(u!=null){
+				id=u.getId();
 			}
 
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new AppException("com.qiangge.dao.impl.login");
-		} finally {
-			DBUtil.closeResultSet(rs);
-			DBUtil.closeStatement(psmt);
-			DBUtil.closeConnection(conn);
 		}
 		return id;
 
 	}
 
+	/**
+	 * 判断用户是编辑还是管理员
+	 */
 	@Override
 	public int getRoleById(int id) throws AppException {
 		// 初始化用户role
 		int role = -1;
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		conn = DBUtil.getConnection(); // 创建数据库连接
-		// 声明操作语句：将用户信息保存到数据库中， “？”为占位符 注意del
-		String sql = "select role from t_user where id=?;";
+
 		try {
-			psmt = conn.prepareStatement(sql);
-			psmt.setInt(1, id);
-			rs = psmt.executeQuery();
-			while (rs.next()) {
-				role = rs.getInt(1);
+			String hql = "from User u where u.id=?;";
+			Session session = HibernateUtil.getSession();
+			// 执行查询
+			Query query = session.createQuery(hql);
+			query.setInteger(0, id);
+			Integer roleNum = (Integer) query.uniqueResult();
+			if (roleNum != null) {
+				role = roleNum;
 			}
 
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new AppException("com.qiangge.dao.impl.getRoleById");
-		} finally {
-			DBUtil.closeResultSet(rs);
-			DBUtil.closeStatement(psmt);
-			DBUtil.closeConnection(conn);
 		}
 		return role;
 	}
